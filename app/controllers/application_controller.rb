c2261fef4cdaa80b350b613b5d329e4b122aee97
EC2 Default User <ec2-user@ip-172-31-3-199.ec2.internal>
2020-09-13 18:45:12 +0000

    part 1 complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  def title_header
-    Movie.order('title')
-  end
+  #def title_header
   
-  def release_date_header
-    Movie.order("release_date")
-  end
+  #Attempt 2:
+  #  Movie.order('title')
+  #end
+  
+  #def release_date_header
+  #  Movie.order("release_date")
+  #end
     
   protect_from_forgery with: :exception
 end
