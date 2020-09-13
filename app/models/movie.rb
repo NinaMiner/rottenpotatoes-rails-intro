20d64e537a4d31d95622bf38ed8fe4596455d332
EC2 Default User <ec2-user@ip-172-31-3-199.ec2.internal>
2020-09-13 22:19:36 +0000

    message

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,8 @@ class Movie < ActiveRecord::Base
     #usingthe active record refused to work
  #has_many :movies, ->{order "title"}
  #has_many :movies, ->{order "release_date"}
- 
+ #build a class to define particular ratings 
  def self.ratings
-    pluck('DISTINCT rating').sort!
+    Movie.select(:rating).distinct.inject([]){|a, m| a.push m.rating}
  end
 end
